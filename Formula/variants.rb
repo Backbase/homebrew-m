890f02203874c5b31e3b638cd836c7bc06931916
Arthur Alves <arthuralves.p@gmail.com>
2020-06-10 13:52:43 +0200

    variants pre-release 0.0.1

---
+++ Formula/variants.rb
@@ -0,0 +1,13 @@
+class Variants < Formula
+  desc "Setup deployment variants and full CI/CD pipelines for mobile projects"
+  homepage "https://github.com/backbase/variants.git"
+  url "https://github.com/backbase/variants.git", tag: "0.0.1"
+  version "0.0.1"
+
+  depends_on :macos
+  depends_on :xcode
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+end
