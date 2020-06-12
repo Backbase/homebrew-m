7c7314c41a8a8ceed75c2786940ef9e953788e12
Arthur Alves <arthuralves.p@gmail.com>
2020-06-12 16:08:10 +0200

    variants release 0.0.3

--- Formula/variants.rb
+++ Formula/variants.rb
@@ -1,8 +1,8 @@
 class Variants < Formula
   desc "Setup deployment variants and full CI/CD pipelines for mobile projects"
   homepage "https://github.com/backbase/variants.git"
-  url "https://github.com/backbase/variants.git", tag: "0.0.2"
-  version "0.0.2"
+  url "https://github.com/backbase/variants.git", tag: "0.0.3"
+  version "0.0.3"
 
   depends_on :macos
   depends_on :xcode
