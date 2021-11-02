04ff7110c450373d4404d7412d8402709763fc01
Gabriel Minucci <gabriel@minucci.com.br>
2021-11-02 10:47:31 +0100

    variants release 0.9.4

--- Formula/variants0.9.3.rb
+++ Formula/variants0.9.3.rb
@@ -1,8 +1,8 @@
-class Variants < Formula
+class Variants093 < Formula
   desc "Setup deployment variants and fully working CI/CD pipelines for mobile projects"
   homepage "https://github.com/backbase/variants.git"
-  url "https://github.com/backbase/variants.git", tag: "0.9.4"
-  version "0.9.4"
+  url "https://github.com/backbase/variants.git", tag: "0.9.3"
+  version "0.9.3"
 
   depends_on :macos
 
