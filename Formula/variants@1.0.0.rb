56b6e498e4706c2df233b0e725563cec30c0c4d6
Giuseppe D <giuseppe.deraco@gmail.com>
2021-11-05 12:00:47 +0100

    update to 1.0.0

--- Formula/variants@1.0.0.rb
+++ Formula/variants@1.0.0.rb
@@ -1,4 +1,4 @@
-class Variants < Formula
+class VariantsAT094 < Formula
   desc "Setup deployment variants and fully working CI/CD pipelines for mobile projects"
   homepage "https://github.com/backbase/variants.git"
   url "https://github.com/backbase/variants.git", tag: "1.0.0"
@@ -10,3 +10,4 @@ class Variants < Formula
     system "make", "install", "prefix=#{prefix}"
   end
 end
+diff --git a/Formula/variants.rb b/Formula/variants.rb
