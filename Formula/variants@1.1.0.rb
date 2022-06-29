0d355fe07b9f99c58f41bcc204cff6e67d03c245
Shehan Gunarathne <98467157+shehan-bb@users.noreply.github.com>
2022-06-29 10:45:44 +0200

    bump up the version

--- Formula/variants@1.1.0.rb
+++ Formula/variants@1.1.0.rb
@@ -1,4 +1,4 @@
-class Variants < Formula
+class VariantsAT110 < Formula
   desc "Setup deployment variants and fully working CI/CD pipelines for mobile projects"
   homepage "https://github.com/backbase/variants.git"
   url "https://github.com/backbase/variants.git", tag: "1.1.0"
