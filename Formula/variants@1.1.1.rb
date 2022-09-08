99177cd551a42a14b9c8d048d9999c003e487c40
Arthur Alves <arthuralves.p@gmail.com>
2022-09-08 15:01:55 +0200

    chore: add bottle for release 1.1.1

--- Formula/variants@1.1.1.rb
+++ Formula/variants@1.1.1.rb
@@ -3,6 +3,13 @@ class VariantsAT111 < Formula
   homepage "https://github.com/backbase/variants"
   url "https://github.com/backbase/variants.git", tag: "1.1.1-fat"
 
+  bottle do
+    rebuild 1
+    root_url "https://github.com/Backbase/variants/releases/download/1.1.1-fat"
+    sha256 cellar: :any_skip_relocation, monterey:       "37a6e60317886bf704f0313adc850f499b5acf4c7d76f82c8f7df9bcd0118d75"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "37a6e60317886bf704f0313adc850f499b5acf4c7d76f82c8f7df9bcd0118d75"
+  end
+
   depends_on :macos
 
   def install
