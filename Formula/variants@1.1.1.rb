a209cd71f3b6c681daf81fa51830f2f5b76eb881
Arthur Alves <arthuralves.p@gmail.com>
2022-09-23 13:59:51 +0200

    Update variants@1.1.1.rb
    Resolve SHA256 mismatch

--- Formula/variants@1.1.1.rb
+++ Formula/variants@1.1.1.rb
@@ -5,8 +5,8 @@ class VariantsAT111 < Formula
 
   bottle do
     root_url "https://github.com/Backbase/homebrew-m/releases/download/1.1.1"
-    sha256 cellar: :any_skip_relocation, monterey: "390266c6de8abd359fae38304c6ba8c8930c48c6ea062e4954ccf38e7d6d405e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "390266c6de8abd359fae38304c6ba8c8930c48c6ea062e4954ccf38e7d6d405e"
+    sha256 cellar: :any_skip_relocation, monterey: "52a727603ccf6fbb36a103c18ba087959d2c5b41cf670569c5a178d9044ba46b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "52a727603ccf6fbb36a103c18ba087959d2c5b41cf670569c5a178d9044ba46b"
   end
 
   depends_on :macos
