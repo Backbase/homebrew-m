c06018db3cf557e7b79e261fddcda0af95e2e15f
BOSS <oss@backbase.com>
2025-03-03 13:39:15 +0000

    Update formula to Variants version 1.3.1

--- Formula/variants.rb
+++ Formula/variants.rb
@@ -1,12 +1,12 @@
 class Variants < Formula
   desc "Setup deployment variants and fully working CI/CD pipelines for mobile projects"
   homepage "https://github.com/backbase/variants"
-  url "https://github.com/backbase/variants.git", tag: "1.3.0"
+  url "https://github.com/backbase/variants.git", tag: "1.3.1"
 
   bottle do
-    root_url "https://github.com/Backbase/homebrew-m/releases/download/1.3.0"
-    sha256 cellar: :any_skip_relocation, x86_64_dunno: "9f2428fe65bcee0b83509f8a13045c04354cad6566d8cef0cd49ef2743382108"
-    sha256 cellar: :any_skip_relocation, arm64_dunno: "9f2428fe65bcee0b83509f8a13045c04354cad6566d8cef0cd49ef2743382108"
+    root_url "https://github.com/Backbase/homebrew-m/releases/download/1.3.1"
+    sha256 cellar: :any_skip_relocation, x86_64_dunno: "5c919e67df6430b990a99d713560404029e3a633293cbc603e4fc6264bcd7b96"
+    sha256 cellar: :any_skip_relocation, arm64_dunno: "5c919e67df6430b990a99d713560404029e3a633293cbc603e4fc6264bcd7b96"
   end
 
   depends_on :macos
---
+++ Formula/variants@1.3.0.rb
@@ -0,0 +1,17 @@
+class VariantsAT130 < Formula
+  desc "Setup deployment variants and fully working CI/CD pipelines for mobile projects"
+  homepage "https://github.com/backbase/variants"
+  url "https://github.com/backbase/variants.git", tag: "1.3.0"
+
+  bottle do
+    root_url "https://github.com/Backbase/homebrew-m/releases/download/1.3.0"
+    sha256 cellar: :any_skip_relocation, x86_64_dunno: ""
+    sha256 cellar: :any_skip_relocation, arm64_dunno: "9f2428fe65bcee0b83509f8a13045c04354cad6566d8cef0cd49ef2743382108"
+  end
+
+  depends_on :macos
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+end
